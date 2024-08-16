257ba5d74ffc27e3dd6d58e5c75c6020369b0f6b
Gaston095 <Gastonsalas095@gmail.com>
2024-08-16 10:11:01 -0300

    Se instala la libreria CORS para permitir la conexion con el frontend

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 let persons = [
@@ -91,7 +93,7 @@ app.get('/info',(req,res) => {
   `)
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
 })
